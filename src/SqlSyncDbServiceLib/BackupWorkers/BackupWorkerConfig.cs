a286a01e102c45a5cff59d7c3515d8ba62ecee51
tiephoangdev <30700664+TiepHoangDev@users.noreply.github.com>
2024-01-28 23:49:33 +0700

    move to stander lib

---
+++ src/SqlSyncDbServiceLib/BackupWorkers/BackupWorkerConfig.cs
@@ -0,0 +1,50 @@
+﻿using SqlSyncDbServiceLib.Interfaces;
+using System;
+using System.IO;
+
+namespace SqlSyncDbServiceLib.BackupWorkers
+{
+    public class BackupWorkerConfig : WorkerConfigBase
+    {
+        public TimeSpan ResetAtTime { get; set; } = new TimeSpan(4, 0, 0);
+        public DayOfWeek? ResetAtDay { get; set; } = DayOfWeek.Saturday;
+        public DateTime? LastRunBackupFull { get; set; }
+
+        public override void OnUpdateSqlConnectionString(string newValue, string oldValue)
+        {
+            base.OnUpdateSqlConnectionString(newValue, oldValue);
+            DirData = Path.Combine(DirRoot, "backup");
+        }
+
+        public bool IsReset(DateTime now)
+        {
+            if (ResetAtDay.HasValue && now.DayOfWeek != ResetAtDay) return false;
+            if (now.TimeOfDay - ResetAtTime > DelayTime) return false;
+            return true;
+        }
+
+        public bool IsExistBackupFull(string minVersion)
+        {
+            return File.Exists(GetPathBackupFull(minVersion));
+        }
+
+        public string GetPathBackupFull(string version) => GetPathFile(version, "full");
+
+        public string GetPathFile(string minVersion, string version)
+        {
+            var path = Path.Combine(DirData, minVersion, $"{DbName}.{version}.syncdb");
+            return Path.GetFullPath(path);
+        }
+
+        public void DeleteByMinVersion(string minVersion)
+        {
+            var path = GetPathBackupFull(minVersion);
+            var dir = Path.GetDirectoryName(path);
+            if (Directory.Exists(dir)) Directory.Delete(dir, true);
+        }
+
+        public BackupWorkerState GetStateByVersion(string version)
+            => GetStateByVersion<BackupWorkerState>(version);
+
+    }
+}
---
+++ src/SqlSyncDbServiceLib/BackupWorkers/BackupWorkerState.cs
@@ -0,0 +1,18 @@
+﻿using SqlSyncDbServiceLib.Helpers;
+
+namespace SqlSyncDbServiceLib.BackupWorkers
+{
+    public class BackupWorkerState : WorkerStateVersionBase
+    {
+        public const string MinVersion_default = "no_min_version";
+        public string MinVersion { get; set; } = MinVersion_default;
+
+        public override string GetNextVersion<T>(string dir, string currentVersion)
+        {
+            if (currentVersion == null) return MinVersion;
+            if (currentVersion.CompareTo(MinVersion) < 0) return MinVersion;
+            return base.GetNextVersion<T>(dir, currentVersion);
+        }
+
+    }
+}
